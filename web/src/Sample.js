d8469f02c5ebcd8a5be80ca75183fa97e206d5d3
Stephan Zlatarev <stephan.zlatarev@sap.com>
2023-09-03 10:50:19 +0300

    Update visualization of samples

--- web/src/Sample.js
+++ web/src/Sample.js
@@ -15,25 +15,25 @@ export default class Sample extends React.Component {
         objects.push(<rect key={ key++ } x={ x } y={ y } width="10" height="10" stroke="lightGray" strokeWidth="0.1" fill="none" />);
 
         // Own military
-        objects.push(<text key={ key++ } x={ x } y={ y + 10 } fill="green" fillOpacity={ sample.input[spot] } style={{ fontSize: 5 }}>♞</text>);
+        objects.push(<text key={ key++ } x={ x - 1 } y={ y + 10 } fill="green" fillOpacity={ sample.input[spot] } style={{ fontSize: 5 }}>♞</text>);
 
         // Own economy
-        objects.push(<text key={ key++ } x={ x + 5 } y={ y + 10 } fill="green" fillOpacity={ sample.input[spot + 100] } style={{ fontSize: 5 }}>♜</text>);
+        objects.push(<text key={ key++ } x={ x + 6 } y={ y + 10 } fill="green" fillOpacity={ sample.input[spot + 100] } style={{ fontSize: 5 }}>♜</text>);
 
         // Enemy military
-        objects.push(<text key={ key++ } x={ x + 5 } y={ y + 5 } fill="red" fillOpacity={ sample.input[spot + 200] } style={{ fontSize: 5 }}>♞</text>);
+        objects.push(<text key={ key++ } x={ x + 6 } y={ y + 4 } fill="red" fillOpacity={ sample.input[spot + 200] } style={{ fontSize: 5 }}>♞</text>);
 
         // Enemy economy
-        objects.push(<text key={ key++ } x={ x } y={ y + 5 } fill="red" fillOpacity={ sample.input[spot + 300] } style={{ fontSize: 5 }}>♜</text>);
+        objects.push(<text key={ key++ } x={ x - 1 } y={ y + 4 } fill="red" fillOpacity={ sample.input[spot + 300] } style={{ fontSize: 5 }}>♜</text>);
 
         // Expected deployment
-        objects.push(<text key={ key++ } x={ x + 2.5 } y={ y + 7.5 } fill="blue" fillOpacity={ sample.output[spot] } style={{ fontSize: 5 }}>♞</text>);
+        objects.push(<text key={ key++ } x={ x + 3.5 } y={ y + 6 } fill="blue" fillOpacity={ sample.output[spot] } style={{ fontSize: 5 }}>♞</text>);
 
         // Prediction
-        objects.push(<text key={ key++ } x={ x } y={ y + 7.5 } fill="purple" fillOpacity={ sample.prediction[spot] } style={{ fontSize: 5 }}>♞</text>);
+        objects.push(<text key={ key++ } x={ x + 1.5 } y={ y + 8 } fill="#099" fillOpacity={ sample.prediction[spot] } style={{ fontSize: 5 }}>♞</text>);
 
         if (spot === sample.spot) {
-          objects.push(<rect key={ key++ } x={ x } y={ y } width="10" height="10" stroke="red" strokeWidth="0.5" fill="none" />);
+          objects.push(<circle key={ key++ } cx={ x + 4 } cy={ y + 6 } r="2.5" stroke="red" strokeWidth="1" fill="none" />);
         }
 
         const error = Math.abs(sample.prediction[spot] - sample.output[spot]);

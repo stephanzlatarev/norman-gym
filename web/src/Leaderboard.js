d0e80ca999b737e9026ff5c1a7df00cad022456f
Stephan Zlatarev <stephan.zlatarev@sap.com>
2023-09-05 21:37:17 +0300

    Show state of trainers

--- web/src/Leaderboard.js
+++ web/src/Leaderboard.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Avatar from "@mui/material/Avatar";
 import Paper from "@mui/material/Paper";
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
@@ -6,6 +7,7 @@ import TableCell from "@mui/material/TableCell";
 import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
+import StateIcon from '@mui/icons-material/PlayArrow';
 
 export default class Controls extends React.Component {
 
@@ -16,9 +18,11 @@ export default class Controls extends React.Component {
     for (const brain of this.props.rank) {
       const style = (brain.brain === this.props.selected) ?  { backgroundColor: "AliceBlue" } : { cursor: "pointer" };
       const selectBrain = () => { onSelect(brain.brain); };
+      const state = (brain.time > Date.now() - 1000 * 60 * 60 * 2) ? "green" : "gray";
 
       brains.push(
         <TableRow key={ brain.brain } sx={ style } onClick={ selectBrain }>
+          <TableCell><Avatar sx={{ width: 16, height: 16, bgcolor: state }}><StateIcon sx={{ width: 10, height: 10 }} /></Avatar></TableCell>
           <TableCell>{ brain.brain }</TableCell>
           <TableCell>{ brain.shape }</TableCell>
           <TableCell>{ (brain.pass * 100).toFixed(2) }%</TableCell>
@@ -34,6 +38,7 @@ export default class Controls extends React.Component {
         <Table>
           <TableHead>
             <TableRow>
+              <TableCell></TableCell>
               <TableCell>BRAIN</TableCell>
               <TableCell>SHAPE</TableCell>
               <TableCell>PASS</TableCell>
